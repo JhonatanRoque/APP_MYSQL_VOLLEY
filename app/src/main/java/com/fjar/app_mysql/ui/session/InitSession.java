1ab2b1130c85e48c1992e44f580abb3517bb0c49
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-10-17 20:58:41 -0600

    Diseño base de Registro de usuario

--- app/src/main/java/com/fjar/app_mysql/ui/session/InitSession.java
+++ app/src/main/java/com/fjar/app_mysql/ui/session/InitSession.java
@@ -2,15 +2,40 @@ package com.fjar.app_mysql.ui.session;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.fjar.app_mysql.MainActivity;
 import com.fjar.app_mysql.R;
 
 public class InitSession extends AppCompatActivity {
+    private Button btnRegistrar, btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init_session);
+
+        //Instanciamos nuestros componentes
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrarse);
+        btnIniciar = (Button) findViewById(R.id.btnIngresar);
+
+        //Funcion de los botones
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cambiarVista = new Intent(InitSession.this, Register.class);
+                startActivity(cambiarVista);
+            }
+        });
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vista = new Intent(InitSession.this, MainActivity.class);
+                startActivity(vista);
+            }
+        });
     }
 }
